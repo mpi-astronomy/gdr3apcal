25c86381760f6595db314fa06a1b9f788d757a9b
Morgan Fouesneau <mfouesneau@users.noreply.github.com>
2023-05-31 10:32:57 +0200

    Update config.py

--- src/gdr3apcal/config.py
+++ src/gdr3apcal/config.py
@@ -10,7 +10,7 @@ __PACKAGE_DIR__ = '/'.join(os.path.abspath(inspect.getfile(inspect.currentframe(
 
 try:
   from importlib import resources
-  modelsdir = importlib.resources.files('gdr3apcal') / 'models'
+  modelsdir = resources.files('gdr3apcal') / 'models'
 except (ImportError, AttributeError):  # older importlib version
   from pkg_resources import resource_filename
   modelsdir = resource_filename('gdr3apcal', 'models')
